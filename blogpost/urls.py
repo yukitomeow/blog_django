bbe689956b5f65b4e30b21bf1ef4a8f053602019
Yukimi Otagiri <72503145+yukitomeow@users.noreply.github.com>
2021-11-23 19:48:03 +0900

    implemented delete and update

--- blogpost/urls.py
+++ blogpost/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import BlogList, BlogDetail, BlogCreate
+from .views import BlogList, BlogDetail, BlogCreate, BlogDelete, BlogUpdate
 
 urlpatterns = [
     path('list/', BlogList.as_view(), name='list'),
     path('detail/<int:pk>/', BlogDetail.as_view(), name='detail'),
     path('create/', BlogCreate.as_view(), name='create'),
+    path('delete/', BlogDelete.as_view(), name='delete'),
+    path('update/', BlogUpdate.as_view(), name='update')
 ]
-
-
-#
--- blogpost/views.py
+++ blogpost/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic.edit import DeleteView
 from .models import BlogModel
 from django.urls import reverse_lazy
 
@@ -18,6 +19,19 @@ class BlogDetail(DetailView):
 
 class BlogCreate(CreateView):
     template_name = 'create.html'
+    model = BlogModel
+    fields = ('title', 'content', 'category')
+    success_url = reverse_lazy('list')
+
+
+class BlogDelete(DeleteView):
+    template_name = 'delete.html'
+    model = BlogModel
+    success_url = reverse_lazy('list')
+
+
+class BlogUpdate(UpdateView):
+    template_name = 'update.html'
     model = BlogModel
     fields = ('title', 'content', 'category')
     success_url = reverse_lazy('list')
