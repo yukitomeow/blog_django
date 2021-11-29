ccf9a563442d98ae7a6a87281b094ef8838e491a
Yukimi Otagiri <72503145+yukitomeow@users.noreply.github.com>
2021-11-30 00:05:20 +0900

    implemented test_urls.py

--- blogpost/test/test_urls.py
+++ blogpost/test/test_urls.py
@@ -2,9 +2,26 @@ from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from blogpost.views import BlogList, BlogDetail, BlogCreate, BlogDelete, BlogUpdate
 
+# テスト内容は、「特定のURLでの動作が、そのURLのViewと一致しているか」です。
+
 
 class TestUrls(SimpleTestCase):
-    def test_list_uri_is_resolved(self):
+    def test_list_url_is_resolved(self):
         url = reverse('list')
-        # I dont understnd what .func.vew_class is
         self.assertEquals(resolve(url).func.view_class, BlogList)
+
+    def test_detail_url_is_resolved(self):
+        url = reverse('detail', args=[1])
+        self.assertEquals(resolve(url).func.view_class, BlogDetail)
+
+    def test_create_url_is_resolved(self):
+        url = reverse('create')
+        self.assertEquals(resolve(url).func.view_class, BlogCreate)
+
+    def test_delete_url_is_resolved(self):
+        url = reverse('delete', args=[1])
+        self.assertEquals(resolve(url).func.view_class, BlogDelete)
+
+    def test_update_url_is_resolved(self):
+        url = reverse('update', args=[1])
+        self.assertEquals(resolve(url).func.view_class, BlogUpdate)
